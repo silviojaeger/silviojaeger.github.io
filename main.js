974268a73ea28ca64cff3664f01d00a481079138
silviojaeger <silvio.jaeger@hotmail.com>
2018-10-26 14:59:40 +0200

    ServiceWorker

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-function doNothing(){
-    console.log("Hello World");
+document.addEventListener('DOMContentLoaded', function(){loadServiceWorker()})
+
+function loadServiceWorker(){
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+        });
+    }
 }
+
+
